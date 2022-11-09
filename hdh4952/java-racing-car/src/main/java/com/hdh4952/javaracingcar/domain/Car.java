ed140680d3713358a5514364b37634a767828226
hdh4952 <gkseogml4959@gmail.com>
2022-11-09 16:00:33 +0900

    feat : Car

--- hdh4952/java-racing-car/src/main/java/com/hdh4952/javaracingcar/domain/Car.java
+++ hdh4952/java-racing-car/src/main/java/com/hdh4952/javaracingcar/domain/Car.java
@@ -1,5 +1,23 @@
 package com.hdh4952.javaracingcar.domain;
 
 public class Car {
+  final private String name;
+  private int moveCount;
 
+  Car(String name) {
+    this.name = name;
+    moveCount = 0;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getMoveCount() {
+    return moveCount;
+  }
+
+  public void move() {
+    moveCount++;
+  }
 }
